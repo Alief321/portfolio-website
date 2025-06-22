ae90f6749a5f98604f771853d0074b3b131de77b
Alief Zulvan <depanb187@gmail.com>
2025-06-22 18:33:20 +0700

    fix: check analytic

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import Footer from '@/components/footer';
 import ThemeSwitch from '@/components/theme-switch';
 import ThemeContextProvider from '@/context/theme-context';
 import { Toaster } from 'react-hot-toast';
+import Script from 'next/script';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -31,6 +32,17 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             <ThemeSwitch />
           </ActiveSectionContextProvider>
         </ThemeContextProvider>
+
+        {/* <!-- Google tag (gtag.js) --> */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-8ZCQJCFMLS"></script>
+        <Script id="gtag-init" strategy="afterInteractive">
+          {`
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', 'G-8ZCQJCFMLS');
+              `}
+        </Script>
       </body>
     </html>
   );
